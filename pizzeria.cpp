d8aff67d8d83befc210c749869dad406abae8fc2
rodrigojfc <00053714@uca.edu.sv>
2020-01-13 22:27:10 -0600

    contrasenya

--- pizzeria.cpp
+++ pizzeria.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 using namespace std;
 
-void contrasenya(){
+int contrasenya(int choice){
+    char arr1 [] = "fstream";
+    char arr2 [] = "";
+    int correct;
+    if (choice == 1){
+        cout<<"\tIngresar la contrasenya: "<<endl;
+        cin>>arr2;
+    }
+    for (int i = 0; i < 7; i++){
+      cout << arr2[i];
+      if (arr2[i] == arr1[i]){
+        correct = 1;
+        i++;
+      }
+      else if (arr2[i] != arr1[i]){
+        correct = 0;
+        cout << "\tLa contrasenya es invalida" << endl;
+        break;
+      }
+
+    }
+   return 0;
+}
+
+int main(void){
     int seleccion;
+    int pass;
     cout<<"\t1-Ingresar al menu principal"<<endl;
     cout<<"\t2-Salir"<<endl;
     cin>>seleccion;
@@ -10,8 +35,7 @@ void contrasenya(){
         cout<<"\tIngrese numero 1 o 2"<<endl;
         cin>>seleccion;
     }
-}
-
-int main(void){
-    contrasenya();
+    contrasenya(seleccion);
+    
+  
 }
